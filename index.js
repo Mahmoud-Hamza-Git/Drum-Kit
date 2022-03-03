838eaaca9fc1fa888eaa72c880a1a6a7f1aa8dc9
MahmoudHamza <mahmoudhamza9440@gmail.com>
2022-03-03 22:14:31 +0200

    Revert "code cleaning and final style"
    This reverts commit 8a8ddb311192fdc53e3409a47e15d74cc16d5fd7.

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0 ; i<numberOfButtons ; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        var buttonPressed = this.innerHTML;
-        clickReaction(buttonPressed);
-        buttonAnimation(buttonPressed);
-    }); 
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick ); 
 } //notice that this selector selects the firs button only
 
-document.addEventListener("keydown",function(Event){
-    clickReaction(Event.key);
-    buttonAnimation(Event.key);
-});
+document.addEventListener("keydown",playSound);
 
-
-
-function clickReaction(buttonPressed){
+function handleClick(){
+    this.classList.toggle("black");
+    var buttonPressed = this.innerHTML;
     switch (buttonPressed){
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
@@ -49,11 +42,43 @@ function clickReaction(buttonPressed){
     }
 }
 
-
-
-function buttonAnimation(key){
-    document.querySelector("."+key).classList.add("pressed");
-    setTimeout(() => {
-        document.querySelector("."+key).classList.remove("pressed");
-    }, 100);
+function playSound(Event){
+    switch (Event.key){
+        case "w":
+            document.querySelector(".w").classList.toggle("black");
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "a":
+            document.querySelector(".a").classList.toggle("black");
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "s":
+            document.querySelector(".s").classList.toggle("black");
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "d":
+            document.querySelector(".d").classList.toggle("black");
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            document.querySelector(".j").classList.toggle("black");
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case "k":
+            document.querySelector(".k").classList.toggle("black");
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "l":
+            document.querySelector(".l").classList.toggle("black");
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        default: console.log(Event.key);
+    }
 }
